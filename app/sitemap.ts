2ec07c7fd9e5edf9d3389f5762e829dd42384e8d
vietnam-nightlife <kimjongseo0403@gmail.com>
2026-09-07 21:10:06 +0700

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -42,6 +42,18 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "weekly",
       priority: 0.8,
     },
+    {
+      url: `${baseUrl}/hanoi/massage/gangnam-spa`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.8,
+    },
+    {
+      url: `${baseUrl}/hanoi/massage/moon-massage`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.8,
+    },
 
     // 가라오케
     {
